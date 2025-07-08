689ff3fb78cc62a9f33c4399545ae2b831ce4e46
Razan <razanalsamra@gmail.com>
2025-07-08 15:17:20 +0300

    refactor: redesign home page

--- obj/Debug/net8.0/CompetitionsWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/CompetitionsWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompetitionsWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da423cdc12f054f0a06f88bc3abb2c05bdbc4278")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+825acdf2df9b8998fa981a5cb6228e1767a1e3c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompetitionsWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompetitionsWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
